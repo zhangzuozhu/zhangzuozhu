5ca29b1a4a7d41ec543c85855afb6ca7c7c00b86
zhangzuozhu <yangsen711@gmail.com>
2020-10-26 15:12:15 +0800

    test1

--- gitDemo/src/main/java/com/zzz/Test.java
+++ gitDemo/src/main/java/com/zzz/Test.java
@@ -3,5 +3,6 @@ package com.zzz;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hehe");
+        System.out.println("\"heheda\" = " + "heheda");
     }
 }
