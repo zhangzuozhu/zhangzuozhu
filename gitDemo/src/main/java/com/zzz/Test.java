ace4d3b08b70971ac6ce6c2dada43d04adc12707
zhangzuozhu <yangsen711@gmail.com>
2020-10-26 15:13:21 +0800

    Delete Test.java

--- gitDemo/src/main/java/com/zzz/Test.java
+++
@@ -1,8 +0,0 @@
-package com.zzz;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hehe");
-        System.out.println("\"heheda\" = " + "heheda");
-    }
-}
